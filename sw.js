1c177f0a28a174dadeb0e0d379d2a5eeca88b46d
rorocamilo76-oss <rorocamilo76@gmail.com>
2026-05-06 22:51:56 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,45 +1,22 @@
-// KOAJ – Service Worker Corregido
-const CACHE_NAME = 'koaj-v1';
-const ASSETS = [
-  './',
-  './index.html',
-  './style.css',
-  './script.js',
-  './icon.svg',
-  './manifest.json'
+// KOAJ – Service Worker
+const CACHE = 'koaj-v1';
+const ARCHIVOS = [
+  '/BogotaVerde/',
+  '/BogotaVerde/index.html',
+  '/BogotaVerde/style.css',
+  '/BogotaVerde/script.js',
+  '/BogotaVerde/icon.svg',
+  '/BogotaVerde/manifest.json'
 ];
 
-// Instalación: Guardar archivos en caché
 self.addEventListener('install', e => {
   e.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => {
-        console.log('Cache KOAJ abierto');
-        return cache.addAll(ASSETS);
-      })
-      .then(() => self.skipWaiting()) // Fuerza al SW a activarse de inmediato
+    caches.open(CACHE).then(cache => cache.addAll(ARCHIVOS))
   );
 });
 
-// Activación: Limpiar versiones antiguas de caché e iconos
-self.addEventListener('activate', e => {
-  e.waitUntil(
-    caches.keys().then(keys => {
-      return Promise.all(
-        keys.map(key => {
-          if (key !== CACHE_NAME) return caches.delete(key);
-        })
-      );
-    })
-  );
-});
-
-// Estrategia de respuesta
 self.addEventListener('fetch', e => {
   e.respondWith(
-    caches.match(e.request).then(res => {
-      return res || fetch(e.request);
-    })
+    caches.match(e.request).then(res => res || fetch(e.request))
   );
 });
-                 
